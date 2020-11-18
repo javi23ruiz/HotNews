a8c0b2b9820d0668d86d5b63669c000ae85afaa7
Javier Ruiz <javierruiz@Javiers-MacBook-Pro.local>
2020-11-18 23:42:34 +0100

    add parrot class + minor tweaks

---
+++ src/text2speech/get_voice_from_text.py
@@ -0,0 +1,56 @@
+import os 
+import logging
+import time
+import gtts
+
+from datetime import datetime
+from num2words import num2words
+from playsound import playsound
+
+#logging formatting
+log_formatter = '%(asctime)s %(levelname)s %(filename)s(%(lineno)d) ::: %(message)s'
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO, format=log_formatter, datefmt='%d-%m-%y %H:%M:%S')
+
+class Parrot:
+    """Class to convert the summary of the news to a podcast
+        using gTTS (it wraps the Google API for text to speech). """
+    def __init__(self, language='en'):
+        self.language = language
+
+    def generate_audio(self, news_link, keyword, 
+                        download_folder=os.path.join(os.getenv['PROJECT_PATH'], 'podcast')):
+
+        try: 
+            #first concatenate all the text in one string to generate the podcast.
+            start_time = time.time()
+            today = datetime.today()
+            day, week_day, month, year = today.strftime("%d"), today.strftime("%A"), \
+                                        today.strftime("%B"), today.strftime("%Y")
+ 
+            podcast_text = f"Hello dear user. How are you today? \
+                                Welcome to the daily podcast for {keyword}. \
+                                Today´s is  {week_day} the {day}th of {month} of {year}. \
+                                Today´s news are: "
+
+            for num, news in enumerate(news_link):
+                news_text = f"The {num2words(num + 1, to='ordinal_num')} article is: \
+                                        {news['title']}. The summary of this article \
+                                        from the source {news['source']['name']} is the following. "
+                podcast_text += news_text + news['description']
+            # make request to google 
+            podcast = gtts.gTTS(podcast_text, lang=self.language)
+            save_name = "_".join([keyword, day, month, year])
+            podcast.save(f"{save_name}.mp3")
+            logger.info(f"Podcast generated and saved in {round(time.time() - start_time, 4)} seconds")
+            #playsound("hello.mp3")
+            return True
+
+        except Exception as e:
+            raise Exception(f"Error generating the audio in the Parrot class: {e}")
+        
+
+
+if __name__ == '__main__':
+    audio = Parrot()
+    audio.generate_audio()
