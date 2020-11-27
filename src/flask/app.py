16ba2e458337d1240ff3a2b6139f185d0ba40f8b
Javier Ruiz <javierruiz@Javiers-MacBook-Pro.local>
2020-11-27 22:07:26 +0100

    home front page

--- src/flask/app.py
+++ src/flask/app.py
@@ -1,21 +1,28 @@
 import logging 
-
+import os
 from flask import Flask, render_template, request
+
+#app imports
+##app imports
+from api_news.google_news_api import GoogleNews
+from mail_manager.send_mail import EmailSender
+from text2speech.get_voice_from_text import Parrot
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('home.html')
 
-@app.route('/company', methods=['POST'])
+@app.route('/', methods=['POST'])
 def my_email_post():
     if request.method == 'POST':
-        company_name = request.form['company_name']
-        print(company_name)
         email = request.form['email']
         print(email)
-    return render_template('index.html')
+        company_name = request.form['company_name']
+        print(company_name)
+
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
